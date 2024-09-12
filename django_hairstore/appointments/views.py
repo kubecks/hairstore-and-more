bb78be5b1dece57bf38dff66a47c3c7492449b18
romeo kubeka <romeokubeka@gmail.com>
2024-09-12 19:52:44 +0000

    - Created a custom sign-up view in appointments/views.py using Django's UserCreationForm and added signup.html template to handle user registration.

--- django_hairstore/appointments/views.py
+++ django_hairstore/appointments/views.py
@@ -1,8 +1,8 @@
-# appointments/views.py
-
 from django.shortcuts import render, redirect
 from .forms import AppointmentForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 
 # Render the homepage
 def get_homepage(request):
@@ -21,3 +21,16 @@ def book_appointment(request):
     else:
         form = AppointmentForm()
     return render(request, 'appointments/book_appointment.html', {'form': form})
+
+# Handle user signup
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)  # Log the user in after successful registration
+            return redirect('homepage')  # Redirect to the homepage after signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
