00a1e0f0b5582d9eab66a0551915363104d87bd4
romeo kubeka <romeokubeka@gmail.com>
2024-09-12 20:26:36 +0000

    Fix URLs in django_hairstore/urls.py for login, logout, and sign-up.

--- django_hairstore/django_hairstore/urls.py
+++ django_hairstore/django_hairstore/urls.py
@@ -2,9 +2,11 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from appointments.views import signup
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('appointments.urls')),  # Includes all URLs from the appointments app
-    path('accounts/', include('django.contrib.auth.urls')),  # For login and logout
+    path('', include('appointments.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # Includes login/logout views
+    path('signup/', signup, name='signup'),  # Custom sign-up view
 ]
